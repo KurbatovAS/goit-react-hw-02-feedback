fcffd30a5db7e3b49c4f4f176a5d6e16e81c43dd
Andrey Kurbatov <85691791+KurbatovAS@users.noreply.github.com>
2022-02-06 21:51:08 +0200

    Homework is done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,50 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        textTransform: 'uppercase',
-        color: '#010101',
-      }}
-    >
-      React homework template
-    </div>
-  );
+import React from 'react';
+import Section from './Section/Section';
+import FeedbackOptions from './FeedbackOptions/FeedbackOptions';
+import Statistics from './Statistics/Statistics';
+import Notification from './Notification/Notification';
+
+
+class App extends React.Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  onClickHandler = e => {
+    this.setState(prevState => {
+      return { [e.target.name]: (prevState[e.target.name] += 1) };
+    });
+  };
+
+  countTotalFeedback = () => {
+    return this.state.good + this.state.neutral + this.state.bad
+  };
+
+  countPositiveFeedbackPercentage = () => {
+    return this.state.good && Math.round((this.state.good / this.countTotalFeedback()) * 100)    
+  };
+
+  render() {
+    const feedbackValues = Object.keys(this.state);
+    const TotalFeedback = this.countTotalFeedback();
+    const PositiveFeedbackPercentage = this.countPositiveFeedbackPercentage();
+
+    return (
+      <>
+        <Section title="Please leave feedback">
+          <FeedbackOptions options={feedbackValues} onLeaveFeedback={this.onClickHandler} />  
+        </Section>
+        
+        <Section title="Statistics">
+          {TotalFeedback
+            ? <Statistics good={this.state.good} neutral={this.state.neutral} bad={this.state.bad} total={TotalFeedback} positivePercentage={PositiveFeedbackPercentage} />
+            : <Notification message="There is no feedback" />}      
+        </Section>    
+      </>
+    );
+  };
 };
+
+export default App;
